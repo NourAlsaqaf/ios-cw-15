372c50283239712470bb23d05b1d293c6c3cd643
NourAlsaqaf <100691291+NourAlsaqaf@users.noreply.github.com>
2022-03-19 10:38:45 +0300

    done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,9 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Student {
+    var name : String
+    var age : Int
+    var grade : Double
+    var attendence : Bool
+}
+Student ( name: "nour" , age: 16 , grade: 90 , attendence: true )
